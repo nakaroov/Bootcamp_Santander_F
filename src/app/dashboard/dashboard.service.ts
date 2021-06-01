643b5b49036efb60a2c63c7e75ecceee4625ec9d
nakaroov <iameduuardo@gmail.com>
2021-06-01 19:30:08 -0300

    first commit

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -7,7 +7,7 @@ import Stock from '../shared/models/stock-model';
 })
 export class DashboardService {
 
-  readonly baseUrl = 'https://bootcamp-dio.herokuapp.com/bootcamp';
+  readonly baseUrl = 'https://dio-santander-bootcamp-e.herokuapp.com/bootcamp';
 
   constructor(private http: HttpClient) { }
 
